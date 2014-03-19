de72af4039f5b868f3940a063e7e9ede854b44ee
jaketrent <trent.jake@gmail.com>
2014-03-18 19:02:03 -0600

    server that doesnt require express; the things i'll do w/o the internet

--- server/app.js
+++ server/app.js
@@ -1,13 +1,43 @@
-const path = require('path')
-const express = require('express')
+var http = require('http')
+var fs = require('fs')
+var path = require('path')
 
-const port = process.env.PORT || 3000
-const app = express()
+var port = process.env.PORT || 3000
 
-const assetsPath = 'public'
+var assetDir = 'public'
 
-app.get('/', function(req, res) {
-  res.sendfile(path.join(__dirname, assetsPath, 'index.html'))
-}).configure(function() {
-  app.use('/' + assetsPath, express.static(path.join(__dirname, assetsPath)))
+const getMime = function (extName) {
+  const mimes = {
+    '.js': 'text/javascript',
+    '.css': 'text/css',
+    '.jpg': 'image/jpeg',
+    '.png': 'image/png'
+  }
+  return mimes[extName] || 'text/html'
+}
+
+const routeToFilePath = function (url) {
+  if (url === '/')
+    return path.join(__dirname, assetDir, 'index.html')
+  else
+    return path.join(__dirname, assetDir, url)
+}
+
+const serveFile = function (res, filePath) {
+  fs.readFile(filePath, function(err, buffer) {
+    if (err) {
+      console.log(err)
+      res.writeHead(500)
+      res.end()
+    } else {
+      res.writeHead(200, { 'Content-Type': getMime(path.extname(filePath)) })
+      res.end(buffer, 'utf-8')
+    }
+  })
+}
+
+http.createServer(function (req, res) {
+  serveFile(res, routeToFilePath(req.url))
 }).listen(port)
+
+console.log("Listening on " + port)
